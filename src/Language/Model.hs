062a6d1c6b452ce553619734e33991e0885b9ce9
Bram Vandenbogaerde <bram@vdb.space>
2025-07-30 17:36:53 +0200

    wip

--- src/Language/Model.hs
+++ src/Language/Model.hs
@@ -1,17 +1,30 @@
 {-# LANGUAGE ConstraintKinds #-}
 {-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE EmptyDataDecls #-}
 module Language.Model where
 
 import qualified Data.Map as Map
 import Data.Map (Map)
 import Data.Set (Set)
 import Control.Monad.State
+import Control.Lens hiding (Context)
 import Language.AST
+import Control.Monad.Except
 
+-----------------------------------------
+-- Model datatypes
+-----------------------------------------
+
+
+-- | Names to refer to sorts
+newtype SortName = SortName String deriving (Ord, Eq, Show)
+
+-- | Variables
 type Var = String
 
 -- | A data constructor is either an atom or a tag paired with a list of sorts
-data DataCtor = DataAtom String | DataTagged String [Sort]
+data DataCtor = DataAtom String | DataTagged String [SortName]
               deriving (Ord, Eq, Show)
 
 -- | A sort consists of a set of data constructors, a name of the sort and
@@ -19,17 +32,34 @@ data DataCtor = DataAtom String | DataTagged String [Sort]
 data Sort = Sort String (Set Var) (Set DataCtor)
           deriving (Ord, Eq, Show)
 
+-----------------------------------------
+-- Errors
+-----------------------------------------
+
+data ModelError = DuplicateVariable String
+                deriving (Ord, Eq, Show)
+
+-----------------------------------------
+-- Context typing
+-----------------------------------------
+
 -- | A typing context used when parsing the syntax rules
-data Context = Context { atomToSorts :: Map String Sort, -- ^ mapping from data atoms (in variables or datactors) to their sorts
-                         functorToCtor :: Map String DataCtor,
-                         sorts :: Map String Sort        -- ^ a mapping from a sort name to their sort data structure
+data Context = Context { _atomToSorts   :: Map String Sort, -- ^ mapping from data atoms (in variables or datactors) to their sorts
+                         _functorToCtor :: Map String DataCtor,
+                         _sorts         :: Map String Sort  -- ^ a mapping from a sort name to their sort data structure
                        } deriving (Ord, Eq, Show)
 
+$(makeLenses ''Context)
 
 -- | Monad for modifying and tracking the typing context
-type MonadTy m = (MonadState Context m)
+type MonadTy m = (MonadState Context m, MonadError ModelError m)
         
--- | Add the contents of a single syntax rule Contextto the typing context 
+-- | Associate a single 
+
+-- | Add the contents of a single syntax rule Context to the typing context 
 addSyntaxRule :: MonadTy m => SyntaxDecl -> m ()
-addSyntaxRule = undefined
+addSyntaxRule (SyntaxDecl vars tpy ctors) = do
+  -- associate a variable with the sort, ensuring that no other
+  -- variables are already associated with it
+  mapM (assocAtomToSort tpy) vars 
 
