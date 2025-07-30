bbfac3ce2bea6213ea0c946f243eacd3672535b7
Bram Vandenbogaerde <bram@vdb.space>
2025-07-30 19:11:23 +0200

    fix(model): perform two passes for registering `syntax` declarations

--- app/Main.hs
+++ app/Main.hs
@@ -3,12 +3,15 @@ module Main where
 import Prelude hiding (lex)
 import Language.Lexer
 import Language.Parser
+import Language.Model
 import Text.Pretty.Simple
+import Data.Either
 
 main :: IO ()
 main = do
   contents <- readFile "example.sem"
   let tokens = lex emptyCtx contents
   -- pPrint tokens
-  pPrint $ runParser tokens
+  let ast = fromRight (error "could not parse program") $  runParser tokens
+  pPrint $ testContext ast
 
