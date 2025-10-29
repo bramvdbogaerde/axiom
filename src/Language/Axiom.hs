7157d149a7aa618d189e0c6397c45b810e0a80f6
Bram Vandenbogaerde <bram@vdb.space>
2025-10-29 13:32:14 +0100

    feat(prelude): added type checking function to the prelude

--- src/Language/Axiom.hs
+++ src/Language/Axiom.hs
@@ -3,16 +3,22 @@ module Language.Axiom(
     module Language.CodeGen.Prelude,
     solverFromRules,
     solve,
+    typeQuery
   ) where 
 
 import Language.CodeGen.Prelude
 import qualified Language.Solver as Solver
 import qualified Language.Solver.BacktrackingST as ST
 import Data.Map
+import Language.TypeCheck
 
 -- | Create a new solver based on the given rules and subtyping information
 solverFromRules :: (ForAllPhases Ord p) => Subtyping -> [RuleDecl' p] -> [PureRewriteDecl p] -> Solver.EngineCtx p [] s
 solverFromRules = Solver.fromRules
+
+-- | Type an untyped term using the given type checker
+typeQuery :: CheckingContext -> PureTerm -> Either Error TypedTerm
+typeQuery = runCheckTerm
 
 -- | Solve 'query' against 'solver' and return the out-cache of the given query
 solve :: (HaskellExprExecutor p, AnnotateType p, ForAllPhases Ord p, HaskellExprRename p)
