335d1184ab1777dede596ed963b02173283d2b83
Bram Vandenbogaerde <bram@vdb.space>
2025-11-04 09:48:12 +0100

    move monadic interface to its own module

--- src/Language/SolverNew.hs
+++ src/Language/SolverNew.hs
@@ -4,227 +4,30 @@
 {-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE TypeApplications #-}
 
-module Language.SolverNew where
+module Language.SolverNew(
+    EngineCtx,
+    fromProgram
+  , runSolver) where
 
-import Control.Applicative (Alternative(..))
 import Control.Lens
 import Control.Monad (MonadPlus(..), msum, when, unless)
 import Control.Monad.Except (catchError)
 import Control.Monad.Extra (ifM)
 import Control.Monad.State (StateT, get, gets, modify, evalStateT)
 import Control.Monad.Trans (lift)
-import Data.Map (Map)
-import qualified Data.Map as Map
-import Data.Maybe (mapMaybe, isJust)
-import Data.Set (Set)
+import Data.Maybe (isJust)
 import qualified Data.Set as Set
 import Data.Proxy
-import qualified Data.List as List
 import Language.AST
 import qualified Language.Solver.BacktrackingST as ST
 import qualified Language.Solver.Renamer as Renamer
 import Language.Solver.Unification (RefTerm, UnificationM)
 import qualified Language.Solver.Unification as Unification
 import Language.Solver.Worklist (Queue(..))
-import Language.TypeCheck
 import Language.Types
 import qualified Debug.Trace as Debug
-
--------------------------------------------------------------
--- Solver monad with free monad structure
--------------------------------------------------------------
-
--- | Free monad functor for search with branching
-data SearchF p s next
-  = -- | Create a branch point: both alternatives to try
-    Branch next next
-  | -- | Failure: this search branch failed
-    Fail
-  | -- | Execute a unification action
-    Unify (UnificationM p s next)
-  | -- | Pop the first goal from the goal list (returns Nothing if empty)
-    PopGoal (Maybe (SearchGoal p s) -> next)
-  | -- | Add goals to the current goal list
-    AddGoals [SearchGoal p s] next
-  | -- | Get the in-cache (whenSucceeds) for the current search state
-    GetInCache ([PureTerm' p] -> next)
-  | -- | Add a term to the in-cache
-    AddToInCache (PureTerm' p) next
-  | -- | Lookup a term in the out-cache
-    LookupCache (PureTerm' p) (Set (PureTerm' p) -> next)
-  | -- | Add a term to the out-cache with its result
-    AddToOutCache (PureTerm' p) (PureTerm' p) next
-  | -- | Find rules matching a functor name
-    FindRules String ([RuleDecl' p] -> next)
-  deriving (Functor)
-
--- | The solver monad as a free monad over SearchF
-data Solver p s a
-  = Pure a
-  | Free (SearchF p s (Solver p s a))
-
-instance Functor (Solver p s) where
-  fmap f (Pure a) = Pure (f a)
-  fmap f (Free fa) = Free (fmap (fmap f) fa)
-
-instance Applicative (Solver p s) where
-  pure = Pure
-  Pure f <*> Pure a = Pure (f a)
-  Pure f <*> Free fa = Free (fmap (fmap f) fa)
-  Free ff <*> a = Free (fmap (<*> a) ff)
-
-instance Monad (Solver p s) where
-  Pure a >>= f = f a
-  Free fa >>= f = Free (fmap (>>= f) fa)
-
--- | Lift a unification action into the solver monad
-liftUnification :: UnificationM p s a -> Solver p s a
-liftUnification m = Free (Unify (fmap Pure m))
-
--- | Lift an ST action into the solver monad
-liftST :: ST.ST s a -> Solver p s a
-liftST = liftUnification . Unification.liftST
-
--- | Create a branch point with two alternatives
--- Both branches should produce the same result type
-branch :: Solver p s a -> Solver p s a -> Solver p s a
-branch left right = Free (Branch left right)
-
--- | Represent failure in the search
-failSearch :: Solver p s a
-failSearch = Free Fail
-
--- | Pop the first goal from the goal list
-popGoal :: Solver p s (Maybe (SearchGoal p s))
-popGoal = Free (PopGoal Pure)
-
--- | Add goals to the current goal list
-addGoals :: [SearchGoal p s] -> Solver p s ()
-addGoals goals = Free (AddGoals goals (Pure ()))
-
--- | Get the in-cache for the current search state
-getInCache :: Solver p s [PureTerm' p]
-getInCache = Free (GetInCache Pure)
-
--- | Add a term to the in-cache
-addToInCache :: PureTerm' p -> Solver p s ()
-addToInCache term = Free (AddToInCache term (Pure ()))
-
--- | Lookup a term in the out-cache
-lookupCache :: PureTerm' p -> Solver p s (Set (PureTerm' p))
-lookupCache term = Free (LookupCache term Pure)
-
--- | Add a term to the out-cache with its result
-addToOutCache :: PureTerm' p -> PureTerm' p -> Solver p s ()
-addToOutCache key value = Free (AddToOutCache key value (Pure ()))
-
--- | Find rules matching a functor name
-findRules :: String -> Solver p s [RuleDecl' p]
-findRules name = Free (FindRules name Pure)
-
--- | Alternative instance for non-deterministic search
--- empty represents failure, <|> represents branching
-instance Alternative (Solver p s) where
-  empty = failSearch
-  (<|>) = branch
-
--- | MonadPlus instance
-instance MonadPlus (Solver p s) where
-  mzero = empty
-  mplus = (<|>)
-
--------------------------------------------------------------
--- Solver context & Search states
--------------------------------------------------------------
-
--- | A new goal for search, keeps track of the term
--- to be solved and also from which rule this term
--- originated.
-data SearchGoal p s = SearchGoal
-  { _goalRuleName :: String,
-    _goalTerm :: RefTerm p s
-  }
-
-data SearchState p s = SearchState
-  { -- | Unique identifier for this search state
-    _searchStateId :: Int,
-    -- | Remaining goals to be solved
-    _searchGoals :: ![SearchGoal p s],
-    -- | Snapshot to restore when resuming this search
-    _searchSnapshot :: ST.Snapshot s,
-    -- | Goals to add to the cache when all the search goals in this state
-    -- have been solved. (i.e., in-cache)
-    _searchWhenSucceeds :: ![PureTerm' p]
-  }
-
--- | A queued search: pairs a computation with its search state
--- Uses existential quantification to hide the result type
-data QueuedSearch p s = forall a. QueuedSearch
-  { _queuedComputation :: !(Solver p s a),
-    _queuedState :: !(SearchState p s)
-  }
-
-$(makeLenses ''SearchState)
-
-data EngineCtx p q s = EngineCtx
-  { -- | Mapping from functor names to their appropriate rule, used in backwards reasoning
-    _conclusionFunctors :: !(Map String (Set (RuleDecl' p))),
-    -- | Queue of states that need to be considered
-    _searchQueue :: !(q (QueuedSearch p s)),
-    -- | Out-caching mechanism as a mapping from terms to ground terms
-    _outCache :: !(Map (PureTerm' p) (Set (PureTerm' p))),
-    -- | Information about rewrite rules
-    _rewriteRules :: Unification.Rewrites p,
-    -- | Set of rules declared with "on" for forward reasoning
-    -- represented as a map and indexed by the name of the facts
-    -- in the precedent.
-    _onRules :: Map String (Set (RuleDecl' p)),
-    -- | Subtyping graph for type-aware unification
-    _subtyping :: !Subtyping,
-    -- | The current state of the search
-    _currentSearchState :: !(SearchState p s)
-  }
-
-$(makeLenses ''EngineCtx)
-
--------------------------------------------------------------
--- Context construction
--------------------------------------------------------------
-
--- | Create an empty engine context with initial search state
-emptyEngineCtx :: (Queue q) => Subtyping -> EngineCtx p q s
-emptyEngineCtx subtyping = EngineCtx
-  { _conclusionFunctors = Map.empty
-  , _searchQueue = emptyQueue
-  , _outCache = Map.empty
-  , _rewriteRules = Map.empty
-  , _onRules = Map.empty
-  , _subtyping = subtyping
-  , _currentSearchState = SearchState 0 [] undefined []  -- snapshot will be set when needed
-  }
-
--- | Associate a functor name with a rule
-addConclusionFunctor :: (ForAllPhases Ord p) => String -> RuleDecl' p -> EngineCtx p q s -> EngineCtx p q s
-addConclusionFunctor nam decl =
-  over conclusionFunctors (Map.insertWith Set.union nam (Set.singleton decl))
-
--- | Construct an initial solver engine context from a program
-fromProgram :: forall q s p. (Queue q, ForAllPhases Ord p)
-            => Subtyping    -- ^ subtyping context for the given program
-            -> Program' p   -- ^ the program to create a context for
-            -> EngineCtx p q s
-fromProgram subtyping (Program decls _) =
-     (flip $ foldr visitRewrite) rewrites
-   $ (flip $ foldr visitRule) rules
-   $ emptyEngineCtx subtyping
-  where rules    = [r | RulesDecl _ rulesDecl _ <- decls, r <- rulesDecl]
-        rewrites = [r | Rewrite r _ <- decls]
-        visitRule rule@(RuleDecl _ _precedent consequents _) =
-          flip (foldr (`addConclusionFunctor` rule)) (mapMaybe functorName (List.singleton (head consequents)))
-        visitRule rule@(OnRuleDecl _ precedents _ _) =
-          flip (foldr (`addConclusionFunctor` rule)) (mapMaybe functorName (List.singleton (head precedents)))
-        visitRewrite decl@(RewriteDecl nam _ _ _) =
-          over rewriteRules (Map.insertWith (++) nam [decl])
+import Language.Axiom.Solver.Monad
+import Data.Either (isRight)
 
 -------------------------------------------------------------
 -- Solver helper functions
@@ -252,8 +55,7 @@ doesUnify t1 t2 = do
   snapshot <- liftST ST.snapshot
   result <- unify t1 t2
   liftST $ ST.restore snapshot
-  return $ either (const False) (const True) result
-
+  return $ isRight result
 
 -------------------------------------------------------------
 -- Core search functions
@@ -391,7 +193,8 @@ processRule goal isInCache rule = do
                   let unificationGoal = SearchGoal ("Csq-" ++ ruleName)
                         (Eqq goal refConsequent (typeAnnot (Proxy @p) AnyType) dummyRange)
 
-                  -- Add other consequents to in-cache
+                  -- Add other consequents to in-cache, so that they get added to the out-cache
+                  -- once the precedents are solved.
                   mapM_ addToInCache renamedOtherConsequents
 
                   if null renamedPrecedents
@@ -431,129 +234,11 @@ processRule goal isInCache rule = do
       addGoals [eqqGoal]
 
 -------------------------------------------------------------
--- Interpreter: run the free monad and manage search states
--------------------------------------------------------------
-
--- | Run a single step of the solver computation
--- When we encounter a Branch, we capture the snapshot and in-cache,
--- enqueue the right branch, and continue with the left
--- Returns Nothing if this branch failed
-runSolverStep :: (Queue q, ForAllPhases Ord p) => Solver p s a -> StateT (EngineCtx p q s) (UnificationM p s) (Maybe (Either a (Solver p s a)))
-runSolverStep (Pure a) = return (Just (Left a))
-
-runSolverStep (Free Fail) = return Nothing  -- This branch fails, try next from queue
-
-runSolverStep (Free (Branch left right)) = do
-  -- Take a snapshot for the right branch
-  snapshot <- lift $ Unification.liftST ST.snapshot
-
-  -- Get current state information
-  ctx <- get
-  let currentState = ctx ^. currentSearchState
-  let inCache = currentState ^. searchWhenSucceeds
-  let goals = currentState ^. searchGoals
-
-  -- Create search state for the right branch
-  let rightState = SearchState 0 goals snapshot inCache
-  let rightQueued = QueuedSearch right rightState
-
-  -- Enqueue only the right branch
-  modify (over searchQueue (enqueue rightQueued))
-
-  -- Continue immediately with the left branch
-  return (Just (Right left))
-
-runSolverStep (Free (Unify m)) = do
-  a <- lift m
-  return (Just (Right a))
-
-runSolverStep (Free (PopGoal k)) = do
-  ctx <- get
-  let goals = ctx ^. currentSearchState . searchGoals
-  case goals of
-    [] -> return (Just (Right (k Nothing)))
-    (g:gs) -> do
-      -- Update the goals in the current search state
-      modify (over currentSearchState (set searchGoals gs))
-      return (Just (Right (k (Just g))))
-
-runSolverStep (Free (AddGoals newGoals k)) = do
-  modify (over currentSearchState (over searchGoals (++ newGoals)))
-  return (Just (Right k))
-
-runSolverStep (Free (GetInCache k)) = do
-  ctx <- get
-  let inCache = ctx ^. currentSearchState . searchWhenSucceeds
-  return (Just (Right (k inCache)))
-
-runSolverStep (Free (AddToInCache term k)) = do
-  modify (over currentSearchState (over searchWhenSucceeds (term :)))
-  return (Just (Right k))
-
-runSolverStep (Free (LookupCache term k)) = do
-  ctx <- get
-  let cache = ctx ^. outCache
-  -- Find entries in cache that unify with the term (simplified for now)
-  let results = Map.findWithDefault Set.empty term cache
-  return (Just (Right (k results)))
-
-runSolverStep (Free (AddToOutCache key value k)) = do
-  modify (over outCache (Map.insertWith Set.union key (Set.singleton value)))
-  return (Just (Right k))
-
-runSolverStep (Free (FindRules name k)) = do
-  ctx <- get
-  let rules = Set.toList $ Map.findWithDefault Set.empty name (ctx ^. conclusionFunctors)
-  return (Just (Right (k rules)))
-
--- | Run a solver computation to completion, looping until we get a result or it fails
--- Returns Nothing if this branch failed
-runSolverToCompletion :: (Queue q, ForAllPhases Ord p) => Solver p s a -> StateT (EngineCtx p q s) (UnificationM p s) (Maybe a)
-runSolverToCompletion solver = do
-  result <- runSolverStep solver
-  case result of
-    Nothing -> return Nothing  -- This branch failed
-    Just (Left a) -> return (Just a)  -- Got a result
-    Just (Right nextSolver) -> runSolverToCompletion nextSolver  -- Continue
-
--- | Dequeue the next search state and run it
--- Returns Nothing if the queue is empty
-dequeueAndRun :: (Queue q, ForAllPhases Ord p) => StateT (EngineCtx p q s) (UnificationM p s) (Maybe ())
-dequeueAndRun = do
-  ctx <- get
-  case dequeue (ctx ^. searchQueue) of
-    Nothing -> return Nothing  -- Queue is empty
-    Just (QueuedSearch computation state, restQueue) -> do
-      -- Update the queue
-      modify (set searchQueue restQueue)
-
-      -- Restore the snapshot from the dequeued state
-      lift $ Unification.liftST $ ST.restore (state ^. searchSnapshot)
-
-      -- Update current search state
-      modify (set currentSearchState state)
-
-      -- Run the computation to completion
-      -- If this branch fails, that's okay - we'll just try the next one from the queue
-      _ <- runSolverToCompletion computation
-
-      return (Just ())
-
--- | Run all queued searches until the queue is empty
--- This processes all branches in the search tree
-runAllQueued :: (Queue q, ForAllPhases Ord p) => StateT (EngineCtx p q s) (UnificationM p s) ()
-runAllQueued = do
-  maybeMore <- dequeueAndRun
-  case maybeMore of
-    Nothing -> return ()  -- Queue empty, done
-    Just () -> runAllQueued  -- Continue processing
-
--------------------------------------------------------------
 -- High-level API
 -------------------------------------------------------------
 
 -- | Process all queued searches (ignoring their results, just for side effects like caching)
-processAllQueued :: (Queue q, ForAllPhases Ord p) => StateT (EngineCtx p q s) (UnificationM p s) [Bool]
+processAllQueued :: (Queue q, ForAllPhases Ord p, ForAllPhases Show p) => StateT (EngineCtx p q s) (UnificationM p s) [Bool]
 processAllQueued = do
   ctx <- get
   case dequeue (ctx ^. searchQueue) of
@@ -561,7 +246,7 @@ processAllQueued = do
     Just (QueuedSearch computation state, restQueue) -> do
       modify (set searchQueue restQueue)
       lift $ Unification.liftST $ ST.restore (state ^. searchSnapshot)
-      modify (set currentSearchState state)
+      modify (set currentSearchState (Debug.traceWith (show . _searchGoals) state))
       result <- runSolverToCompletion computation  -- Run for side effects (cache updates)
       fmap (isJust result :) processAllQueued  -- Continue with rest of queue
 
@@ -591,7 +276,7 @@ solveUntilStable pureQuery = do
   -- Check if cache changed
   finalCache <- gets (^. outCache)
 
-  if initialCache == Debug.traceShowId finalCache
+  if initialCache == Debug.traceWith (("out >> " ++) . show) finalCache
     then do
       -- Cache stable - return whether we found a solution
       return (or results)
